13fefb89e3df12176b309f54709556ec2ca164c3
kelly gashi <kellygashi00@gmail.com>
2023-11-16 21:46:03 +0100

    first commit

---
+++ src/se/kellygashi/test/TestTriangle.java
@@ -0,0 +1,10 @@
+package se.kellygashi.test;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class TestTriangle {
+
+    @Test
+    @DisplayName("Making sure ")
+}
