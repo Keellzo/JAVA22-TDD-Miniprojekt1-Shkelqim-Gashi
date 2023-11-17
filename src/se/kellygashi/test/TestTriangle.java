f84f72d763717b37c76198d461e5bd5a47b9510c
kelly gashi <kellygashi00@gmail.com>
2023-11-17 12:02:12 +0100

    done

--- src/se/kellygashi/test/TestTriangle.java
+++ src/se/kellygashi/test/TestTriangle.java
@@ -4,10 +4,13 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import se.kellygashi.main.Triangle;
 
+import java.io.ByteArrayInputStream;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 
 public class TestTriangle {
+
 
     @Test
     @DisplayName("Testing input with length more than 3")
@@ -26,6 +29,52 @@ public class TestTriangle {
     }
 
 
+    @Test
+    @DisplayName("Testing getUserInput with valid input")
+    public void getUserInputValid() {
+        String simulatedUserInput = "3,4,5\n";
+        ByteArrayInputStream bais = new ByteArrayInputStream(simulatedUserInput.getBytes());
+        System.setIn(bais);
+
+        Triangle triangle = new Triangle();
+        triangle.getUserInput();
+
+        assertEquals(Triangle.TYPE.SCALENE, triangle.getCurrent_type(), "Triangle should be SCALENE for sides 3, 4, 5");
+
+        System.setIn(System.in);
+    }
+
+
+
+    @Test
+    @DisplayName("Testing getUserInput with invalid input")
+    public void getUserInputInvalid() {
+        String simulatedUserInput = "*,y,p\n";
+        ByteArrayInputStream bais = new ByteArrayInputStream(simulatedUserInput.getBytes());
+        System.setIn(bais);
+
+        Triangle triangle = new Triangle();
+        triangle.getUserInput();
+
+        assertNull(triangle.getCurrent_type(), "Current type should be null for invalid input");
+
+        System.setIn(System.in);
+    }
+
+    @Test
+    @DisplayName("Testing getUserInput with invalid input length")
+    public void getUserInputLengthInvalid() {
+        String simulatedUserInput = "2,2,4,5\n";
+        ByteArrayInputStream bais = new ByteArrayInputStream(simulatedUserInput.getBytes());
+        System.setIn(bais);
+
+        Triangle triangle = new Triangle();
+        triangle.getUserInput();
+
+        assertNull(triangle.getCurrent_type(), "Current type should be null for invalid input");
+
+        System.setIn(System.in);
+    }
 
 
     @Test
@@ -35,6 +84,13 @@ public class TestTriangle {
         String triangleType = String.valueOf(t.getCurrent_type());
 
         assertEquals("ISOSCELES", triangleType);
+    }
+
+    @Test
+    @DisplayName("Boundary test: Triangle with two sides equal to the third")
+    public void getCurrentTypeForTriangleEquality() {
+        Triangle t = new Triangle(1, 1, 2);
+        assertNull(t.getCurrent_type(), "Triangle with sides 1, 1, and 2 should not be valid");
     }
 
     @Test
@@ -72,5 +128,4 @@ public class TestTriangle {
 
         assertEquals("null", triangleType);
     }
-
 }
